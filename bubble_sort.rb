583c974f01985995fe220a7e3a85134494a835d3
Stricks1 <gmalheiross@gmail.com>
2020-05-07 17:36:11 -0300

    fix linter error

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,14 +1,14 @@
 def bubble_sort(arr)
   loop do
-     flag = false
-     for i in 0 ... arr.length
-       next if i == arr.length - 1
-       if (arr[i] > arr[i+1])
-         arr[i], arr[i+1] = arr[i+1], arr[i]
-         flag = true
-       end
-     end
-     break if !flag
+    flag = false
+    arr.each_with_index do |val,i|
+      next if i == arr.length - 1
+      if (arr[i] > arr[i+1])
+        arr[i], arr[i+1] = arr[i+1], arr[i]
+        flag = true
+      end
+    end
+    break if !flag
   end
   arr
 end
--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -1,18 +1,18 @@
 def bubble_sort_by(arr)
-    loop do
-        flag = false
-        for i in 0 ... arr.length   
-          next if i == arr.length - 1
-          if (yield(arr[i], arr[i+1]) > 0)
-            arr[i], arr[i+1] = arr[i+1], arr[i]
-            flag = true
-          end
-        end
-        break if !flag
+  loop do
+    flag = false
+    for i in 0 ... arr.length   
+      next if i == arr.length - 1
+      if (yield(arr[i], arr[i+1]) > 0)
+        arr[i], arr[i+1] = arr[i+1], arr[i]
+        flag = true
+      end
     end
-    puts arr.inspect
+    break if !flag
+  end
+  puts arr.inspect
 end
 
 bubble_sort_by (["hi","hello","hey"]) do |left, right|
-    left.length - right.length
+  left.length - right.length
 end

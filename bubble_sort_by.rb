36899946516aa1ec16b19f7670aaf000f18fa495
rindra josia <rrindrajosia@gmail.com>
2020-05-07 23:53:46 +0300

    fix linter error

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -4,7 +4,7 @@ def bubble_sort_by(arr)
     arr.each_with_index do |_val, i|
       next if i == arr.length - 1
 
-      if yield (arr[i], arr[i + 1]).positive?
+      if yield(arr[i], arr[i + 1]) > 0
         arr[i], arr[i + 1] = arr[i + 1], arr[i]
         flag = true
       end

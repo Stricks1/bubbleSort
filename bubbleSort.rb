810f079d6640ceb4537277d49c2bcfdf324efa7d
Stricks1 <gmalheiross@gmail.com>
2020-05-07 16:31:07 -0300

    change the way swap array elements

--- bubbleSort.rb
+++ bubbleSort.rb
@@ -7,9 +7,7 @@ def bubble_sort(arr)
 
        next if i == arr.length - 1
        if (arr[i] > arr[i+1])
-         temp = arr[i]
-         arr[i] = arr[i+1]
-         arr[i+1] = temp
+         arr[i], arr[i+1] = arr[i+1], arr[i]
          flag = true
       end
 

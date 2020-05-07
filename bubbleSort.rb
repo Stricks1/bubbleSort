66cc18978bd95710e224e83a78024cb627f212da
rindra josia <rrindrajosia@gmail.com>
2020-05-07 22:54:59 +0300

    add buble_sort_by block

--- bubbleSort.rb
+++ bubbleSort.rb
@@ -1,3 +1,4 @@
+
 def bubble_sort(arr)
 
   loop do
@@ -19,33 +20,33 @@ def bubble_sort(arr)
      arr
 end
 
+puts bubble_sort([4,3,78,2,0,2]).inspect
+
+
 def bubble_sort_by(arr)
     loop do
         flag = false
-   
+
         for i in 0 ... arr.length
-   
+
           next if i == arr.length - 1
-          if (arr[i] > arr[i+1])
+          if (yield(arr[i], arr[i+1]))
+
             arr[i], arr[i+1] = arr[i+1], arr[i]
             flag = true
          end
-   
+
         end
-   
+
         break if !flag
-   
+
         end
-        arr
+
+      puts arr.inspect
 end
 
-bubble_sort_by ([4,3,78,2,0,2]) do
+bubble_sort_by ([4,3,78,2,0,2]) do |left, right|
+
+  left > right
 
 end
-
-
-
-
-puts bubble_sort([4,3,78,2,0,2]).inspect
-
-day {puts 'yield'}

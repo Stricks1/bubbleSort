3e0792da445071429bd7caf0922c2f2bed0e467e
Stricks1 <gmalheiross@gmail.com>
2020-05-07 17:28:53 -0300

    fixing stickler

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,23 +1,16 @@
-
 def bubble_sort(arr)
-
   loop do
      flag = false
-
      for i in 0 ... arr.length
-
        next if i == arr.length - 1
        if (arr[i] > arr[i+1])
          arr[i], arr[i+1] = arr[i+1], arr[i]
          flag = true
-      end
-
+       end
      end
-
      break if !flag
-
-     end
-     arr
+  end
+  arr
 end
 
 puts bubble_sort([4,3,78,2,0,2])

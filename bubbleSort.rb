da7c0993318fc6d8aef40928cb0d036e173e4106
Stricks1 <gmalheiross@gmail.com>
2020-05-07 16:46:30 -0300

    yeld

--- bubbleSort.rb
+++ bubbleSort.rb
@@ -20,9 +20,26 @@ def bubble_sort(arr)
 end
 
 def bubble_sort_by(arr)
+    loop do
+        flag = false
+   
+        for i in 0 ... arr.length
+   
+          next if i == arr.length - 1
+          if (arr[i] > arr[i+1])
+            arr[i], arr[i+1] = arr[i+1], arr[i]
+            flag = true
+         end
+   
+        end
+   
+        break if !flag
+   
+        end
+        arr
+end
 
-
-
+bubble_sort_by ([4,3,78,2,0,2]) do
 
 end
 

8c0571355c6e7a36e7a973290629c1672175c173
Stricks1 <gmalheiross@gmail.com>
2020-05-07 17:01:09 -0300

    add second file for sortBy method

--- bubbleSort.rb
+++ bubbleSort.rb
@@ -21,32 +21,3 @@ def bubble_sort(arr)
 end
 
 puts bubble_sort([4,3,78,2,0,2]).inspect
-
-
-def bubble_sort_by(arr)
-    loop do
-        flag = false
-
-        for i in 0 ... arr.length
-
-          next if i == arr.length - 1
-          if (yield(arr[i], arr[i+1]))
-
-            arr[i], arr[i+1] = arr[i+1], arr[i]
-            flag = true
-         end
-
-        end
-
-        break if !flag
-
-        end
-
-      puts arr.inspect
-end
-
-bubble_sort_by ([4,3,78,2,0,2]) do |left, right|
-
-  left > right
-
-end
---
+++ bubbleSortBy.rb
@@ -0,0 +1,27 @@
+def bubble_sort_by(arr)
+    loop do
+        flag = false
+
+        for i in 0 ... arr.length
+         
+          next if i == arr.length - 1
+          if (yield(arr[i], arr[i+1]) > 0)
+
+            arr[i], arr[i+1] = arr[i+1], arr[i]
+            flag = true
+         end
+
+        end
+
+        break if !flag
+
+        end
+
+      puts arr.inspect
+end
+
+bubble_sort_by (["hi","hello","hey"]) do |left, right|
+
+    left.length - right.length
+
+end
